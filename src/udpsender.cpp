5def480d59f0925f8b9fe37fa4a591afd08b334d
Martin Insulander <martin@insulander.com>
2023-04-06 16:55:29 +0200

    Increase default transmit power to 100 (from 10)

--- src/udpsender.cpp
+++ src/udpsender.cpp
@@ -18,11 +18,11 @@ void UDPSender::send()
        << ",LON=" << XPLMGetDatad(drefLongitude) << ",HGT=" << (XPLMGetDataf(drefHeight) * 3.28084)
        << ",COM1_PTT=" << ((ptt && comsel == 6) ? 1 : 0)
        << ",COM1_FRQ=" << (XPLMGetDatai(drefCom1Frequency) / 1000.0)
-       << ",COM1_VOL=" << XPLMGetDataf(drefCom1Volume) << ",COM1_PWR=10.0,COM1_SQC=0.1,COM1_CWKHZ=8.33"
+       << ",COM1_VOL=" << XPLMGetDataf(drefCom1Volume) << ",COM1_PWR=100.0,COM1_SQC=0.1,COM1_CWKHZ=8.33"
        << ",COM1_PBT=" << (XPLMGetDatai(drefCom1Power) && XPLMGetDatai(drefCom1Listening))
        << ",COM2_PTT=" << ((ptt && comsel == 7) ? 1 : 0)
        << ",COM2_FRQ=" << (XPLMGetDatai(drefCom2Frequency) / 1000.0)
-       << ",COM2_VOL=" << XPLMGetDataf(drefCom2Volume) << ",COM2_PWR=10.0,COM2_SQC=0.1,COM2_CWKHZ=8.33"
+       << ",COM2_VOL=" << XPLMGetDataf(drefCom2Volume) << ",COM2_PWR=100.0,COM2_SQC=0.1,COM2_CWKHZ=8.33"
        << ",COM2_PBT=" << (XPLMGetDatai(drefCom2Power) && XPLMGetDatai(drefCom2Listening))
        << ",DEBUG_SIGQLY=-1.0,AUDIO_FX_RADIO=1,AUDIO_HEAR_ALL=0"
        << "\n";
